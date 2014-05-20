e050fe0f296d522fa7e85e9ac9f67ee826a318f9
杨智勇 <lien1992@users.noreply.github.com>
2014-05-20 23:58:07 +0800

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -6,14 +6,12 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=3;i>0;i--){
 			for(int i1=3;i1>0;i1--){
 				
 				System.out.println("world zhiliao");
 	                      	System.out.println("world zhiliao");
 
 			}
-		}
 	}
 
 }
