6c43896dd02d437496489cff174b381df329d216
lien1992 <colorsky1992@gmail.com>
2014-05-21 16:52:34 +0800

    modify

--- src/test.java
+++ src/test.java
@@ -4,14 +4,15 @@ public class test {
 	/**
 	 * @param args
 	 */
-	private int j=0;
+	private static int j=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
                System.err.println("Error !");
-               for(int i=4;i<9;i_++){
+               for(int i=4;i<9;i++){
                	j++;
+                System.err.println("Number"+j+"  Error !");
                }
-               System.err.println("Number"+j+"Error !");
+              
 	}
 
 }
