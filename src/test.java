9cee1d191baef1d84ba89791fa46f38cbfde4b3e
杨智勇 <lien1992@users.noreply.github.com>
2014-05-21 10:49:07 +0800

    add floop

--- src/test.java
+++ src/test.java
@@ -4,9 +4,14 @@ public class test {
 	/**
 	 * @param args
 	 */
+	private int j=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+               System.err.println("Error !");
+               for(int i=4;i<9;i_++){
+               	j++;
+               }
+               System.err.println("Number"+j+"Error !");
 	}
 
 }
