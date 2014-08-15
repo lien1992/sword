c82451c62108a9ea6bbcff577db57f0577a52802
John Young <lien1992@users.noreply.github.com>
2014-08-15 22:55:19 +0800

    更新hello
    在hello里加了一个重复的输出。

--- src/Hello.java
+++ src/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
+		System.err.println("Number"+j+"  Error !");
 
 	}
 
